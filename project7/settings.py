075941792882f4f1743b80f6bb4d79640b89ea7c
Ayan2004 <86516841+Ayan2004@users.noreply.github.com>
2023-05-24 21:19:24 +0530

    d

--- project7/settings.py
+++ project7/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'project7.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASE_URL = {postgresql://postgres:BfD8TVpw9nbKMVWDzAv3@containers-us-west-60.railway.app:6738/railway}
+DATABASE_URL = {'postgresql://postgres:BfD8TVpw9nbKMVWDzAv3@containers-us-west-60.railway.app:6738/railway'}
 
 DATABASES = {
      'default': {

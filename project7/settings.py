79beaeae501caff8d34047153c56436c241a9ee9
Ayan2004 <86516841+Ayan2004@users.noreply.github.com>
2023-05-24 16:39:35 +0530

    Update settings.py

--- project7/settings.py
+++ project7/settings.py
@@ -79,12 +79,16 @@ WSGI_APPLICATION = 'project7.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 
 # Password validation
